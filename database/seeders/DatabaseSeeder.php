4dec4ad1bfc1b1c8cc7dbd362082883185e187b6
Everton Lima <eolimabr@gmail.com>
2022-07-02 13:43:29 -0300

    Ajustes de Versão e composer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         $this->call(FornecedorSeeder::class);
         $this->call(SiteContatoSeeder::class);
         $this->call(MotivoContatoSeeder::class);
